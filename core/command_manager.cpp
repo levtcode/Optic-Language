f0f4c9295f7cb7ae644174cf6657f8aa3e2217c7
levtcode <levtcode@gmail.com>
2026-07-31 03:38:47 +0200

    refactor: added a command table to verify if a command exists, created a command signal system to manages the instructions passed between modules

--- core/command_manager.cpp
+++ core/command_manager.cpp
@@ -2,31 +2,40 @@
 
 #include "command_manager.hpp"
 
-#include <string>
+#include <string_view>
 using namespace optic_cli;
 
-/* */
 void optic_cli::display_help(bool is_error) {
     // TODO
 }
 
 /* */
-bool optic_cli::match_code(state_codes_ _code_) {
-    switch (_code_) {
-        case HELP_SIGNAL:
-            display_help(false);
-            return false;
+void optic_cli::handle_signal(const CommandSignal signal) {
+    switch (signal) {
+    case (CommandSignal::ShowHelp):
+        display_help(false);
+        break;
+    case (CommandSignal::ShowErrorHelp):
+        display_help(true);
+        break;
     }
 }
 
-/* */
-state_codes_ optic_cli::read_args(const int argc, const char *const argv[]) {
-    for (int i = 1; i < argc; i++) {
-        const std::string arg = argv[i];
+bool optic_cli::match_arg(const std::string_view &arg) {
+    return command_table.find(arg) != command_table.end();
+}
 
-        if (arg == "help" || arg == "--help") {
-            display_help(false);
-            return HELP_SIGNAL;
+/* */
+CommandSignal optic_cli::read_args(const int argc, const char *const argv[]) {
+    for (int i = 1; i < argc; i++) {
+        const std::string_view arg = argv[i];
+
+        if (match_arg(arg)) {
+            // TODO
+        } else {
+            return CommandSignal::ShowErrorHelp;
         }
     }
+
+    return CommandSignal::Continue;
 }
--- core/core.cpp
+++ core/core.cpp
@@ -17,12 +17,6 @@ int OpticCore::run(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    optic_cli::state_codes_ _code_ = optic_cli::read_args(--argc, ++argv);
-    if (_code_ != 0) {
-        return optic_cli::match_code(_code_);
-    }
-
-    // Load Optic modules (compiler, runtime, etc...)
-
+    
     return EXIT_SUCCESS;
 }
